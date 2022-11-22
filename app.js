0eb9022d5af5ed4a675203949d3360f035213ad1
tekenscodr <ensleybdarku@gmail.com>
2022-11-22 19:44:36 +0000

    las las

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT =  3000
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
