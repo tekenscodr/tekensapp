caa4c71f1d1887e13b4874d7a8b71e5fdb5036de
tekenscodr <ensleybdarku@gmail.com>
2022-12-11 13:13:33 +0000

    header v1.0.1

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads/', express.static('uploads'))
 
-const corsOption = {
-    origin: ['https://tekensapp.vercel.app/'],
-};
- app.use(cors(corsOption));
+// const corsOption = {
+//     origin: ['https://tekensapp.vercel.app/'],
+// };
+//  app.use(cors(corsOption));
 //if you want in every domain then
 app.use(cors())
 
