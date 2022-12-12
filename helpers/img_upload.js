6c4f4646a801a7bea0401c1764b3e5688725fd0c
tekenscodr <ensleybdarku@gmail.com>
2022-12-12 14:57:32 +0000

    uploads

--- helpers/img_upload.js
+++ helpers/img_upload.js
@@ -2,14 +2,15 @@ const path = require('path')
 const multer = require('multer')
 
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/')
-    },
+ const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, path.resolve(__dirname, `..${path.sep}..${path.sep}`, `${path.sep}utils${path.sep}uploads`));
+        },
     filename: function(req, file, cb) {
         let ext = path.extname(file.originalname)
         cb(null, Date.now() + ext)
-    }
+    },
+   
 })
 
 const upload = multer({
