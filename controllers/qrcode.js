c3c90527321e32d576a00efb4b5a015c330e857e
tekenscodr <ensleybdarku@gmail.com>
2022-12-23 13:37:42 +0000

    dels code

--- controllers/qrcode.js
+++ controllers/qrcode.js
@@ -34,6 +34,9 @@ const saveTicket = async(req, res, next) =>{
         const code = await QRCode
             .toFile(`./codegenerated/${ticket.id}.png`, `${userId} + ${eventId}`)
             return res.status(200).json(code) 
+        // const code = await QRCode
+        // .toFile("./codegenerated/npp.png", 'https://newpatrioticparty.org/press-conference-by-the-new-patriotic-party-addressed-by-the-general-secretary/')
+        // return res.status(200).json(code) 
     } catch (err) {
         res.send(err.message)
         next(err)
