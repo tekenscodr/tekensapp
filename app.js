a3e3da04ac5e44fe9ab9482397402b2cfe302764
tekenscodr <ensleybdarku@gmail.com>
2023-02-26 23:18:17 +0000

    dot env

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const createError = require('http-errors')
 // const Customer = require('./models/customer')
-require('dotenv').config()
-// require('./helpers/init_mongo')
-// const { verifyAccessToken } = require('./helpers/jwt_helper')
-// const { getEvents } = require('./controllers/events')
-// require('./helpers/init_redis')
+require('./helpers/init_mongo')
+const { verifyAccessToken } = require('./helpers/jwt_helper')
+const { getEvents } = require('./controllers/events')
+require('./helpers/init_redis')
 const AuthRoute = require('./routes/auth')
 const Events = require('./routes/events')
 const Qrcode = require('./routes/qrcode')
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express()
 //middleware
