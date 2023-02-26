40d8e2e51c8dd92853fadc3d52d2bb8e6755b155
tekenscodr <ensleybdarku@gmail.com>
2023-02-26 23:26:46 +0000

    error gone

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
-const createError = require('http-errors')
+// const createError = require('http-errors')
 // const Customer = require('./models/customer')
 require('./helpers/init_mongo')
-const { verifyAccessToken } = require('./helpers/jwt_helper')
-const { getEvents } = require('./controllers/events')
+// const { verifyAccessToken } = require('./helpers/jwt_helper')
+// const { getEvents } = require('./controllers/events')
 require('./helpers/init_redis')
-const AuthRoute = require('./routes/auth')
-const Events = require('./routes/events')
-const Qrcode = require('./routes/qrcode')
+// const AuthRoute = require('./routes/auth')
+// const Events = require('./routes/events')
+// const Qrcode = require('./routes/qrcode')
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 require('dotenv').config();
@@ -44,23 +44,23 @@ app.get('/', (req, res)=>{
 //         }
 
 // })
-app.use('/auth', AuthRoute)
-app.use('/events', Events)
-app.use('/ticket', Qrcode)
+// app.use('/auth', AuthRoute)
+// app.use('/events', Events)
+// app.use('/ticket', Qrcode)
 
-app.use(async(req, res, next) => {
-    next(createError.NotFound())
-})
+// app.use(async(req, res, next) => {
+//     next(createError.NotFound())
+// })
 
-app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message,
-        },
-    })
-})
+// app.use((err, req, res, next) => {
+//     res.status(err.status || 500)
+//     res.send({
+//         error: {
+//             status: err.status || 500,
+//             message: err.message,
+//         },
+//     })
+// })
 
 const PORT =  6000
 
