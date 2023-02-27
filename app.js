842d034c027fa83f74d4bb531940212b5917e6f8
tekenscodr <ensleybdarku@gmail.com>
2023-02-27 00:06:31 +0000

    yoyoy0

--- app.js
+++ app.js
@@ -62,34 +62,34 @@ app.get('/', (req, res)=>{
 // })
 
 //DATABASE CONNECTION
-mongoose.connect(process.env.MONGODB_URI, {
-    dbName: process.env.DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// mongoose.connect(process.env.MONGODB_URI, {
+//     dbName: process.env.DB_NAME,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
 
-})
-.then(() => {
-    console.log('mongodb tekens connected')
-})
-.catch((err) => console.log(err.message))
+// })
+// .then(() => {
+//     console.log('mongodb tekens connected')
+// })
+// .catch((err) => console.log(err.message))
 
-mongoose.connection.on('connected', () => {
-    console.log('mongodb connection established')
-})
+// mongoose.connection.on('connected', () => {
+//     console.log('mongodb connection established')
+// })
 
-mongoose.connection.on('error', (err) =>{
-    console.log(err.message)
-})
+// mongoose.connection.on('error', (err) =>{
+//     console.log(err.message)
+// })
 
-mongoose.connection.on('disconnected', () => {
-    console.log('mongoose connection disconnected')
-})
+// mongoose.connection.on('disconnected', () => {
+//     console.log('mongoose connection disconnected')
+// })
 
 
-process.on('SIGINT', async () => {
-    await mongoose.connection.close()
-    process.exit(0)
-})
+// process.on('SIGINT', async () => {
+//     await mongoose.connection.close()
+//     process.exit(0)
+// })
 
 const PORT =  6000
 
