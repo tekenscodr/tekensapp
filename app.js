645fda6ec5e7581255ec286e3bcbb70dcff2a5fd
tekenscodr <ensleybdarku@gmail.com>
2023-02-27 00:09:44 +0000

    push

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require('express')
 // const Events = require('./routes/events')
 // const Qrcode = require('./routes/qrcode')
 const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
