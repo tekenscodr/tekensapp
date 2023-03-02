bfd6802df7caed51c07b6f57c7bbc263392c926c
tekenscodr <ensleybdarku@gmail.com>
2023-03-02 16:09:57 +0000

    mor

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 //middleware
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
