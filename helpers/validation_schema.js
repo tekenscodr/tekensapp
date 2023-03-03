54d3837e112bb7498fbecfec320310d4b42c49f7
tekenscodr <ensleybdarku@gmail.com>
2023-03-03 17:43:20 +0000

    pick

--- helpers/validation_schema.js
+++ helpers/validation_schema.js
@@ -22,8 +22,8 @@ const authSchema = Joi.object({
 })
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().lowercase(),
-    password: Joi.string().min(8)
+    email: Joi.string().email().lowercase().required(),
+    password: Joi.string().min(8).required()
 })
 
 module.exports = {
