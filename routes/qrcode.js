ffef7def06e2b71148ad723fb526f2d86fe7d95e
tekenscodr <ensleybdarku@gmail.com>
2024-06-21 10:30:10 +0100

    add-changes-qrcode-routes

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const Ticket = require('../controllers/qrcode')
 const {verifyAccessToken} = require('../helpers/jwt_helper')
 
-
-router.post('/:eventId/:referenceId', verifyAccessToken, Ticket.saveTicket)
+router.get('/attended', verifyAccessToken, Ticket.attended) 
 router.get('/pending', verifyAccessToken, Ticket.unscannedTicket)
-router.get('/scanner', Ticket.scannerTicket) //gets organisers scanned tickets
-router.get('/pay/:userId/:price', Ticket.buyTicket)
+router.get('/canceled', verifyAccessToken, Ticket.canceledTicket)
+router.get('/scanner', Ticket.scannerTicket) 
 router.post('/transferTicket', verifyAccessToken, Ticket.updateTicket)
-router.get('/:id', verifyAccessToken, Ticket.eachTicket);
-router.get('/scanned', verifyAccessToken, Ticket.attended) //get users attended events 
-router.get('/canceled', verifyAccessToken, Ticket.canceledTicket) //get users attended events 
+router.post('/save/:eventId/:referenceId', verifyAccessToken, Ticket.saveTicket)
+router.get('/pay/:userId/:price', Ticket.buyTicket)
+router.get('/getTicket/:id', verifyAccessToken, Ticket.eachTicket);
+
 // CUSTOMER ROUTES **CANCEL**ATTENDED/SCANNED**CURRENT/UPCOMING
 router.put('cancel-ticket/:tickeId', verifyAccessToken, Ticket.cancelTicket)
 
